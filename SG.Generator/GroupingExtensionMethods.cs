0686859fac772abfbea1cbd731de9a0e357fc406
Mark Schofield <34426337+MarkSchofield@users.noreply.github.com>
2023-05-27 16:32:06 -0700

    Add 'GroupingExtensionMethods.Deconstruct' for deconstructing an IGrouping

---
+++ SG.Generator/GroupingExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿namespace SG.Generator
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal static class GroupingExtensionMethods
+    {
+        public static void Deconstruct<TGroup, TItem>(this IGrouping<TGroup, TItem> grouping, out TGroup group, out IEnumerable<TItem> items)
+        {
+            group = grouping.Key;
+            items = grouping;
+        }
+    }
+}
